9a45ed6636b4fc53e1564a59133a612b18f701ea
Brandon Barksdale <bbark93@gmail.com>
2024-11-27 00:42:35 -0500

    checkUsernameExists

--- api/auth/auth-middleware.js
+++ api/auth/auth-middleware.js
@@ -1,4 +1,4 @@
-const User = require('../users/users-model')
+const User = require("../users/users-model");
 /*
   If the user does not have a session saved in the server
 
@@ -8,8 +8,8 @@ const User = require('../users/users-model')
   }
 */
 function restricted(req, res, next) {
-  console.log('restricted');
-  next()
+  console.log("restricted");
+  next();
 }
 
 /*
@@ -22,11 +22,11 @@ function restricted(req, res, next) {
 */
 async function checkUsernameFree(req, res, next) {
   try {
-    const users = await User.findBy({ username: req.body.username })
-    if (!users.length) next()
-    else next({ "message": "Username taken" })
+    const users = await User.findBy({ username: req.body.username });
+    if (!users.length) next();
+    else next({ message: "Username taken", status: 422 });
   } catch (err) {
-    next(err)
+    next(err);
   }
 }
 
@@ -38,8 +38,14 @@ async function checkUsernameFree(req, res, next) {
     "message": "Invalid credentials"
   }
 */
-function checkUsernameExists(req, res, next) {
-  next()
+async function checkUsernameExists(req, res, next) {
+  try {
+    const users = await User.findBy({ username: req.body.username });
+    if (users.length) next();
+    else next({ message: "Invalid credentials", status: 401 });
+  } catch (err) {
+    next(err);
+  }
 }
 
 /*
@@ -51,7 +57,7 @@ function checkUsernameExists(req, res, next) {
   }
 */
 function checkPasswordLength(req, res, next) {
-  next()
+  next();
 }
 
 // Don't forget to add these to the `exports` object so they can be required in other modules
@@ -60,4 +66,4 @@ module.exports = {
   checkUsernameFree,
   checkUsernameExists,
   checkPasswordLength,
-}
+};
